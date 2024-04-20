ecb1e57d90639de136c49b6cada0f14dc486216b
surakaplan <129753409+surakaplan@users.noreply.github.com>
2024-04-20 15:41:55 +0300

    part 6

---
+++ yemekss/sqlsinif.cs
@@ -0,0 +1,15 @@
+﻿        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace yemekss
+{
+    public SqlConnection baglanti()
+    {
+        SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-KITLCBK;Initial Catalog=dbo_yemektarifi;Integrated Security=True");
+        baglan.Open();
+        return baglan;
+    }
+}
