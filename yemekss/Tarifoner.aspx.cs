aef2b1b7bdc3e8f87ccd8b4899192f3240806d92
surakaplan <129753409+surakaplan@users.noreply.github.com>
2024-04-20 21:55:36 +0300

    part 12

--- yemekss/Tarifoner.aspx.cs
+++ yemekss/Tarifoner.aspx.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace yemekss
 {
     public partial class Tarifoner : System.Web.UI.Page
     {
+        sqlsinif bgl = new sqlsinif();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,6 +20,18 @@ namespace yemekss
         protected void Button1_Click(object sender, EventArgs e)
         {
 
+            SqlCommand komut = new SqlCommand("INSERT INTO tarif (tarifad, tarifmalzeme, tarifyapilis, tarifresim, tarifsahip, tarifsahipmail) VALUES (@t1, @t2, @t3, @t4, @t5, @t6)", bgl.baglanti());
+
+
+            komut.Parameters.AddWithValue("@t1", TxtTarifAd.Text);
+            komut.Parameters.AddWithValue("@t2", TxtMalzemeler.Text);
+            komut.Parameters.AddWithValue("@t3", TxtYapilis.Text);
+            komut.Parameters.AddWithValue("@t4", FileUpload1.FileName);
+            komut.Parameters.AddWithValue("@t5", TxtTarifOneren.Text);
+            komut.Parameters.AddWithValue("@t6", TxtMailAdresi.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            Response.Write("Tarifiniz Alınmıştır");
         }
 
         protected void TxtMailAdresi_TextChanged(object sender, EventArgs e)
